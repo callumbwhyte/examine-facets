b98c132bebad45d2a26ae0ee038ffc04b64e2fe9
Callum Whyte <hey@callumwhyte.com>
2020-06-05 19:09:12 +0100

    Interfaces for facets within Examine's fluent API

---
+++ src/Examine.Facets/Search/FacetField.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Examine.Facets.Search
+{
+    public class FacetField : IFacetField
+    {
+        public FacetField(string name)
+        {
+            Name = name;
+        }
+
+        public FacetField(string name, string[] values)
+        {
+            Name = name;
+            Values = values;
+        }
+
+        public FacetField(string name, string[] values, int minHits = 0, int maxCount = 0)
+        {
+            Name = name;
+            Values = values;
+            MinHits = minHits;
+            MaxCount = maxCount;
+        }
+
+        public string Name { get; }
+
+        public string[] Values { get; internal set; }
+
+        public int MinHits { get; internal set; }
+
+        public int MaxCount { get; internal set; }
+    }
+}
---
+++ src/Examine.Facets/Search/IFacetField.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examine.Facets.Search
+{
+    public interface IFacetField
+    {
+        string Name { get; }
+        string[] Values { get; }
+        int MinHits { get; }
+        int MaxCount { get; }
+    }
+}
