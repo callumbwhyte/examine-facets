e6995bbb848aee349c55e646366566f34eaaa801
Callum Whyte <hey@callumwhyte.com>
2020-06-05 18:24:06 +0100

    Necessary objects for facet results

---
+++ src/Examine.Facets/FacetValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Examine.Facets
+{
+    public class FacetValue : IFacetValue
+    {
+        public FacetValue(object value, int hits)
+        {
+            Value = value;
+            Hits = hits;
+        }
+
+        public object Value { get; }
+
+        public int Hits { get; }
+    }
+}
