b4242a252f6b59deaa1d0ecec32134a2b2597fe2
Callum Whyte <hey@callumwhyte.com>
2020-06-07 01:40:37 +0100

    Extension methods for getting facets from ISearchResults

---
+++ src/Examine.Facets/SearchResultExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Examine.Facets.Search;
+
+namespace Examine.Facets
+{
+    public static class SearchResultExtensions
+    {
+        /// <summary>
+        /// Get the values for a particular facet in the results
+        /// </summary>
+        public static IFacetResult GetFacet(this ISearchResults searchResults, string field)
+        {
+            if (!(searchResults is IFacetResults facetResults))
+            {
+                throw new Exception("Result does not support facets");
+            }
+
+            facetResults.Facets.TryGetValue(field, out IFacetResult facet);
+
+            return facet;
+        }
+
+        /// <summary>
+        /// Get all of the facets in the results
+        /// </summary>
+        public static IEnumerable<IFacetResult> GetFacets(this ISearchResults searchResults)
+        {
+            if (!(searchResults is IFacetResults facetResults))
+            {
+                throw new Exception("Result does not support facets");
+            }
+
+            return facetResults.Facets.Values;
+        }
+    }
+}
