bb6287d8223a3c6a3e5939f7bb667eeedd7e2fad
Callum Whyte <hey@callumwhyte.com>
2020-06-18 21:49:52 +0100

    Searcher and query for BoboBrowse

---
+++ src/Examine.Facets.BoboBrowse/BoboFacetSearcher.cs
@@ -0,0 +1,23 @@
+﻿using Examine.LuceneEngine;
+using Examine.LuceneEngine.Providers;
+using Examine.LuceneEngine.Search;
+using Examine.Search;
+using Lucene.Net.Analysis;
+using Lucene.Net.Index;
+
+namespace Examine.Facets.BoboBrowse
+{
+    public class BoboFacetSearcher : LuceneSearcher
+    {
+        public BoboFacetSearcher(string name, IndexWriter writer, Analyzer analyzer, FieldValueTypeCollection fieldValueTypes)
+            : base(name, writer, analyzer, fieldValueTypes)
+        {
+
+        }
+
+        public override IQuery CreateQuery(string category = null, BooleanOperation defaultOperation = BooleanOperation.And)
+        {
+            return new BoboFacetQuery(GetSearchContext(), category, LuceneAnalyzer, GetAllIndexedFields(), new LuceneSearchOptions(), defaultOperation);
+        }
+    }
+}
