1b9b9531f72cf2e1791ba1f3af45fcd1fe459404
Callum Whyte <hey@callumwhyte.com>
2020-07-23 22:15:06 +0100

    Search results for MultiFacetLucene

---
+++ src/Examine.Facets.MultiFacets/MultiFacetRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Lucene.Net.Search;
+using MultiFacetLucene;
+using MultiFacetLucene.Configuration;
+
+namespace Examine.Facets.MultiFacets
+{
+    public class MultiFacetRequest
+    {
+        public Query Query { get; set; }
+
+        public int MaxResults { get; set; } 
+
+        public FacetSearcherConfiguration Config { get; set; }
+
+        public List<FacetFieldInfo> Facets { get; set; }
+    }
+}
---
+++ src/Examine.Facets.MultiFacets/MultiFacetSearchResults.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Examine.Facets.LuceneEngine;
+using Examine.LuceneEngine.Search;
+using Lucene.Net.Search;
+using MultiFacetLucene;
+
+namespace Examine.Facets.MultiFacets
+{
+    internal class MultiFacetSearchResults : FacetSearchResults
+    {
+        private ScoreDoc[] _scoreDocs;
+
+        public MultiFacetSearchResults(ISearchContext searchContext, MultiFacetRequest request)
+            : base(searchContext)
+        {
+            PerformSearch(request);
+        }
+
+        public void PerformSearch(MultiFacetRequest request)
+        {
+            using (var multiSearcher = new FacetSearcher(Searcher.IndexReader, request.Config))
+            {
+                var results = multiSearcher.SearchWithFacets(request.Query, request.MaxResults, request.Facets);
+
+                _scoreDocs = results.Hits.ScoreDocs;
+
+                foreach (var facet in results.Facets.GroupBy(x => x.FacetFieldName))
+                {
+                    Facets.Add(facet.Key, CreateFacetResult(facet.ToList()));
+                }
+
+                TotalItemCount = results.Hits.TotalHits;
+            }
+        }
+
+        protected override int GetTotalDocs() => _scoreDocs.Length;
+
+        protected override ISearchResult GetSearchResult(int index)
+        {
+            var scoreDoc = _scoreDocs[index];
+
+            if (scoreDoc == null)
+            {
+                return null;
+            }
+
+            var doc = Searcher.Doc(scoreDoc.Doc);
+
+            if (doc == null)
+            {
+                return null;
+            }
+
+            return CreateSearchResult(doc, scoreDoc.Score);
+        }
+
+        private IFacetResult CreateFacetResult(IEnumerable<FacetMatch> facets)
+        {
+            var values = facets.Select(x => new FacetValue(x.Value, (int)x.Count));
+
+            return new FacetResult(values);
+        }
+    }
+}
