662244855ae5b7a0ddb4a2e5045e9b38d5fee2ba
Callum Whyte <hey@callumwhyte.com>
2020-09-22 00:43:47 +0100

    Query SortFields are public in Examine 1.1.0

--- src/Examine.Facets/LuceneEngine/FacetSearchQuery.cs
+++ src/Examine.Facets/LuceneEngine/FacetSearchQuery.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using Examine.Facets.Search;
+﻿using Examine.Facets.Search;
 using Examine.LuceneEngine.Search;
 using Examine.Search;
 using Lucene.Net.Analysis;
-using Lucene.Net.Search;
 
 namespace Examine.Facets.LuceneEngine
 {
@@ -12,16 +10,10 @@ namespace Examine.Facets.LuceneEngine
     /// </summary>
     public abstract class FacetSearchQuery : LuceneSearchQuery, IFacetQuery
     {
-        private readonly ISearchContext _searchContext;
-
-        public List<SortField> SortFields { get; }
-
         public FacetSearchQuery(ISearchContext searchContext, string category, Analyzer analyzer, string[] fields, LuceneSearchOptions searchOptions, BooleanOperation occurance)
             : base(searchContext, category, analyzer, fields, searchOptions, occurance)
         {
-            _searchContext = searchContext;
 
-            SortFields = new List<SortField>();
         }
 
         ///<inheritdoc/>
@@ -49,74 +41,6 @@ namespace Examine.Facets.LuceneEngine
         protected abstract void FacetInternal(IFacetField field);
 
         ///<inheritdoc/>
-        public new IBooleanOperation OrderBy(params SortableField[] fields)
-        {
-            OrderByInternal(false, fields);
-
-            return base.OrderBy(fields);
-        }
-
-        ///<inheritdoc/>
-        public new IBooleanOperation OrderByDescending(params SortableField[] fields)
-        {
-            OrderByInternal(true, fields);
-
-            return base.OrderBy(fields);
-        }
-
-        /// <summary>
-        /// Register <see cref="SortableField"/> for use within query
-        /// </summary>
-        protected virtual void OrderByInternal(bool descending, params SortableField[] fields)
-        {
-            foreach (var field in fields)
-            {
-                var defaultSort = SortField.STRING;
-
-                switch (field.SortType)
-                {
-                    case SortType.Score:
-                        defaultSort = SortField.SCORE;
-                        break;
-                    case SortType.DocumentOrder:
-                        defaultSort = SortField.DOC;
-                        break;
-                    case SortType.String:
-                        defaultSort = SortField.STRING;
-                        break;
-                    case SortType.Int:
-                        defaultSort = SortField.INT;
-                        break;
-                    case SortType.Float:
-                        defaultSort = SortField.FLOAT;
-                        break;
-                    case SortType.Long:
-                        defaultSort = SortField.LONG;
-                        break;
-                    case SortType.Double:
-                        defaultSort = SortField.DOUBLE;
-                        break;
-                    case SortType.Short:
-                        defaultSort = SortField.SHORT;
-                        break;
-                    case SortType.Byte:
-                        defaultSort = SortField.BYTE;
-                        break;
-                }
-
-                var fieldName = field.FieldName;
-
-                var valueType = _searchContext.GetFieldValueType(fieldName);
-
-                if (valueType?.SortableFieldName != null)
-                {
-                    fieldName = valueType.SortableFieldName;
-                }
-
-                SortFields.Add(new SortField(fieldName, defaultSort, descending));
-            }
-        }
-
         protected override LuceneBooleanOperationBase CreateOp() => new FacetBooleanOperation(this);
 
         ///<inheritdoc/>
