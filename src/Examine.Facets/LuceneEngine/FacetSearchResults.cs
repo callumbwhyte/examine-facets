9f244b75a41bac5b3cdbca49dfe0da3623bc16c1
Callum Whyte <hey@callumwhyte.com>
2020-09-22 00:42:05 +0100

    Search results paging now handled by Examine

--- src/Examine.Facets/LuceneEngine/FacetSearchResults.cs
+++ src/Examine.Facets/LuceneEngine/FacetSearchResults.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Examine.Facets.Search;
-using Examine.LuceneEngine.Providers;
+using Examine.LuceneEngine;
 using Examine.LuceneEngine.Search;
-using Lucene.Net.Documents;
 using Lucene.Net.Search;
 
 namespace Examine.Facets.LuceneEngine
 {
-    public abstract class FacetSearchResults : FacetSearchResultsBase
+    public abstract class FacetSearchResults : LuceneSearchResultsBase, IFacetResults
     {
         public FacetSearchResults(ISearchContext searchContext)
         {
+            Facets = new Dictionary<string, IFacetResult>();
+
             if (!(searchContext.Searcher is IndexSearcher indexSearcher))
             {
                 throw new Exception("Searcher is invalid");
@@ -21,55 +21,12 @@ namespace Examine.Facets.LuceneEngine
             Searcher = indexSearcher;
         }
 
+        ///<inheritdoc/>
+        public IDictionary<string, IFacetResult> Facets { get; }
+
         /// <summary>
         /// Exposes the internal <see cref="IndexSearcher"/>
         /// </summary>
         public IndexSearcher Searcher { get; }
-
-        /// <summary>
-        /// Creates an <see cref="ISearchResult"/> for a given Lucene <see cref="Document"/>
-        /// </summary>
-        protected virtual ISearchResult CreateSearchResult(Document document, float score)
-        {
-            var id = document.Get("id");
-
-            if (string.IsNullOrEmpty(id) == true)
-            {
-                id = document.Get(LuceneIndex.ItemIdFieldName);
-            }
-
-            var result = new SearchResult(id, score, () =>
-            {
-                var fields = document.GetFields();
-
-                var resultVals = new Dictionary<string, List<string>>();
-
-                foreach (var field in fields.Cast<Field>())
-                {
-                    var fieldName = field.Name;
-
-                    var values = document.GetValues(fieldName);
-
-                    if (resultVals.TryGetValue(fieldName, out var resultFieldVals))
-                    {
-                        foreach (var value in values)
-                        {
-                            if (!resultFieldVals.Contains(value))
-                            {
-                                resultFieldVals.Add(value);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        resultVals[fieldName] = values.ToList();
-                    }
-                }
-
-                return resultVals;
-            });
-
-            return result;
-        }
     }
 }
